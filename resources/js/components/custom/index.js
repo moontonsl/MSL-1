8a54a4174e238d0540817ab1a4569905a007fed5
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-05-26 23:28:47 +0800

    Revert "Add custom UI components including Header, Footer, and Carousel"
    This reverts commit 71f7b00c4bb45cfe5661794e08759cb7c65b02da.

--- resources/js/components/custom/index.js
+++
@@ -1,5 +0,0 @@
-/* put global/shared components component only here */
-// components/index.js
-export { default as Header } from './Header/Header';
-export { default as Footer } from './Footer/Footer';
-export { Heading, Text, Img } from './ui/UIComponents';
