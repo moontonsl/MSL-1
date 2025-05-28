f764fd304199e26c68c367acc8a900cd753423c7
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-05-29 00:16:16 +0800

    Disable debug styles and update layout structure.
    Commented out the debug styles in global.scss and modified the layout structure by replacing the root container class. Added new web assets, including logos, icons, and images, to the public folder for enhanced branding and design.

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,10 +1,9 @@
 // resources/js/Layouts/MainLayout.jsx
-
 import { Footer, Header } from '@/Components/index.js';
 
 export default function MainLayout({ children }) {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="app">
             <Header />
             <main>{children}</main>
             <Footer />
