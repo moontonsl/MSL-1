e3607631dc6cdfb7b805db7bf8f76bc107d7bdce
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-06-06 23:25:33 +0800

    feat(Header.jsx): Add account icon to sign-in button and update styles
    (cherry picked from commit 3d8f9f4b815c023e7d4ea89a863a79568f838772)

--- resources/js/Components/Header/Header.jsx
+++ resources/js/Components/Header/Header.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Link, usePage } from '@inertiajs/react';
 import { Menu } from 'lucide-react';
 import styles from './Header.module.scss';
-import Dropdown from '@/Components/Dropdown';
 import { MdAccountCircle } from "react-icons/md";
 
 // Navigation links array (only main nav links)
@@ -47,73 +46,20 @@ const Header = () => {
                         ))}
                     </ul>
                     <div className={styles.authButtons}>
-
-                        {user ? (
-                            <>
-                                <div className="hidden sm:ms-6 sm:flex sm:items-center">
-                                    <div className="relative ms-3">
-                                        <Dropdown>
-                                            <Dropdown.Trigger>
-                                                <span className="inline-flex rounded-md">
-                                                    <button
-                                                        type="button"
-                                                        className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
-                                                    >
-                                                        {user.name}
-
-                                                        <svg
-                                                            className="-me-0.5 ms-2 h-4 w-4"
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            viewBox="0 0 20 20"
-                                                            fill="currentColor"
-                                                        >
-                                                            <path
-                                                                fillRule="evenodd"
-                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                                clipRule="evenodd"
-                                                            />
-                                                        </svg>
-                                                    </button>
-                                                </span>
-                                            </Dropdown.Trigger>
-
-                                            <Dropdown.Content>
-                                                <Dropdown.Link
-                                                    href={route('profile.edit')}
-                                                >
-                                                    Profile
-                                                </Dropdown.Link>
-                                                <Dropdown.Link
-                                                    href={route('logout')}
-                                                    method="post"
-                                                    as="button"
-                                                >
-                                                    Log Out
-                                                </Dropdown.Link>
-                                            </Dropdown.Content>
-                                        </Dropdown>
-                                    </div>
-                                </div>
-                                
-                            </>
-                        ) : (
-                            <>
-                                <Link
-                                    href="/login"
-                                    className={styles.signInBtn}
-                                    onClick={() => setIsMenuOpen(false)}
-                                >
-                                    Sign In
-                                </Link>
-                                <Link
-                                    href="/register"
-                                    className={styles.registerBtn}
-                                    onClick={() => setIsMenuOpen(false)}
-                                >
-                                    Register
-                                </Link>
-                            </>
-                        )}
+                        <Link
+                            href="/login"
+                            className={styles.signInBtn}
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            <MdAccountCircle size={40} />
+                        </Link>
+                        {/*<Link*/}
+                        {/*    href="/register"*/}
+                        {/*    className={styles.registerBtn}*/}
+                        {/*    onClick={() => setIsMenuOpen(false)}*/}
+                        {/*>*/}
+                        {/*    Register*/}
+                        {/*</Link>*/}
                     </div>
                 </nav>
             </div>
