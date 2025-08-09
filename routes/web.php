c60af9ef3168c18365060c18e63e71bed35714b7
Jairuz Paulo Saure <130030318+jaijailovesyou@users.noreply.github.com>
2025-08-09 12:33:34 +0800

    Add Programs page and related assets
    Introduces a new Programs page with supporting layout and images. Updates dependencies to include react-helmet and @tailwindcss/line-clamp, and modifies global styles and header for integration.

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,11 @@ Route::get('/Events', function () {
     return Inertia::render('Events/Events');
 })->name('Events');
 
+//PROGRAMS  ROUTES
+Route::get('/Programs', function () {
+    return Inertia::render('Programs/Programs');
+})->name('Programs');
+
 //EVENT  ROUTES - MCC WATCHFEST REG
 Route::get('/MCCWatchFestReg', function () {
     return Inertia::render('MCCWatchFest/MCCWatchFestReg');
