67c8596cab48f0fe1e09217a9f820378af088bfb
Sam B <mslserp.website1.ph@gmail.com>
2025-08-24 13:23:54 +0800

    Update migration to change position of 'blocked_reason' column in users table

--- database/migrations/2025_08_08_185759_add_blocked_reason_to_users_table.php
+++ database/migrations/2025_08_08_185759_add_blocked_reason_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('blocked_reason')->nullable()->after('state');
+            $table->text('blocked_reason')->nullable()->after('status');
         });
     }
 
