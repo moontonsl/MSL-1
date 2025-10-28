68c9afd22f794fb7dcb55dab9c7d1caf25f6a4ca
Sam B. <mslserp.website1.ph@gmail.com>
2025-10-28 09:10:59 +0800

    Ensure 'msl_events_data' table creation is conditional in migration
    - Added a check to prevent the recreation of the 'msl_events_data' table if it already exists, enhancing migration safety and preventing potential errors during deployment.

--- database/migrations/2025_10_25_120239_create_msl_events_data_table.php
+++ database/migrations/2025_10_25_120239_create_msl_events_data_table.php
@@ -11,24 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('msl_events_data', function (Blueprint $table) {
-            $table->id();
-            $table->string('event_name');
-            $table->string('event_state')->default('Active');
-            $table->string('event_canonical');
-            $table->string('event_logo')->nullable();
-            $table->string('event_title');
-            $table->text('event_subtitle');
-            $table->text('event_content01')->nullable();
-            $table->text('event_content02')->nullable();
-            $table->string('event_img01')->nullable();
-            $table->string('event_img02')->nullable();
-            $table->string('event_img03')->nullable();
-            $table->string('event_img04')->nullable();
-            $table->string('event_img05')->nullable();
-            $table->boolean('is_featured')->default(false);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('msl_events_data')) {
+            Schema::create('msl_events_data', function (Blueprint $table) {
+                $table->id();
+                $table->string('event_name');
+                $table->string('event_state')->default('Active');
+                $table->string('event_canonical');
+                $table->string('event_logo')->nullable();
+                $table->string('event_title');
+                $table->text('event_subtitle');
+                $table->text('event_content01')->nullable();
+                $table->text('event_content02')->nullable();
+                $table->string('event_img01')->nullable();
+                $table->string('event_img02')->nullable();
+                $table->string('event_img03')->nullable();
+                $table->string('event_img04')->nullable();
+                $table->string('event_img05')->nullable();
+                $table->boolean('is_featured')->default(false);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
